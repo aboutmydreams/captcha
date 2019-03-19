3003740f4b04b99a08695f2c1907d28625f187d7
aboutmydreams <aboutmydreams@163.com>
2019-03-19 17:48:50 +0800

    add keras mode

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 
-
-
 import make_captcha,solve_it
 
 def get_my_train_img(times):
@@ -17,5 +15,22 @@ def get_my_train_img(times):
         mig = solve_it.clear_train_img(mig)
         mig.save('train_imgs/{}.png'.format(name))
 
+get_my_train_img(300)
+
+x_train = []
+y_train = []
+classes = 26
+
+train_img_path = 'train_imgs/'
+data_list = os.listdir(train_img_path)
+
+for file in data_list:
+    if file != '.DS_Store':
+        img = Image.open(train_img_path + file)
+        x = make_captcha.get_mode(img)
+        x_train.append(x)
+        y_train.append(file[0])
+
+print(y_train)
 
 
